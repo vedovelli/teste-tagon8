f9e8db992011b7b6cb3c65df0104e571c4f763a1
Vedovelli <vedovelli@gmail.com>
2014-06-20 16:06:28 -0300

    app.js created

---
+++ node/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var Account;
+var mongoose = require('mongoose').connect('mongodb://127.0.0.1/toro');
+var db = mongoose.connection;
+
+app.listen(4730);
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback(){
+
+	/*Models*/
+	var accountSchema = mongoose.Schema({
+	    fullname: String,
+	    username: String,
+	    password: String
+	});
+
+	Account = mongoose.model('Account', accountSchema);
+});
