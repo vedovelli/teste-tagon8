996cb6c9ab7ca99d079ea66f18f68252fcd71258
Vedovelli <vedovelli@gmail.com>
2014-06-26 11:38:22 -0300

    Alterado o packages.json para contemplar os middlewares necessários para cookie session
    Adicionado o suporte à sessions no express

--- node/app.js
+++ node/app.js
@@ -2,12 +2,20 @@ var express = require('express');
 
 var app = module.exports = express();
 
+var cookieParser = require('cookie-parser');
+
+var cookieSession = require('cookie-session');
+
 var bodyParser = require('body-parser');
 
 /*app setup and startup*/
 app.listen(4730);
 
 app.use(bodyParser.json());
+app.use(cookieParser('$rt6&98.'));
+app.use(cookieSession({
+  secret: '$rt6&98.'
+}));
 
 app.use(bodyParser.urlencoded({
 
