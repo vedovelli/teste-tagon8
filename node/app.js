53370aa2429223bb30e1e62f29aa5a7242ec00ce
Vedovelli <vedovelli@gmail.com>
2014-06-27 08:02:39 -0300

    CORS habilitado na API

--- node/app.js
+++ node/app.js
@@ -21,3 +21,11 @@ app.use(bodyParser.urlencoded({
 
 	extended: true
 }));
+
+/*CORS*/
+app.all('*', function(req, res, next) {
+
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	next();
+});
