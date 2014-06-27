d4505077305286eb2a57078b0d95a90ade9753f6
Vedovelli <vedovelli@gmail.com>
2014-06-27 15:05:30 -0300

    helper para decodificar HTML vindo do MongoDB

--- src/js/helper.js
+++ src/js/helper.js
@@ -6,10 +6,18 @@ Handlebars.registerHelper("log", function(context) {
 
 /* Formata a data padrão Javascript para o padrão 'um dia atrás' ou 'alguns segundos atrás'*/
 Ember.Handlebars.helper('format-date', function(date){
+
 	moment.lang('pt-BR')
 	var markup ='';
 	markup += ' <small>';
 	markup += moment(date).calendar();
 	markup += '</small>';
-	return new Handlebars.SafeString( markup );
+	return new Handlebars.SafeString(markup);
+});
+
+Ember.Handlebars.helper('decode-html', function(content) {
+
+	var e = document.createElement('div');
+	e.innerHTML = content;
+	return new Handlebars.SafeString(e.childNodes[0].nodeValue);
 });
