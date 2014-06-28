d59a3ee21931bb839fa9cb90db94375a5a6dcae7
Vedovelli <vedovelli@gmail.com>
2014-06-28 09:58:13 -0300

    Acao de deixar comentário implementada. Ainda em desenvolvimento

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,9 +2,38 @@ App.PostController = Ember.ObjectController.extend({
 
 	actions: {
 
-		save: function() {
+		saveComment: function() {
 
-			console.log(this.get('model'));
+			var self = this;
+			var postid = this.get('model').get('id');
+
+			var commentObj = {
+				'postid': postid,
+				fullname: this.get('fullname'),
+				email: this.get('email'),
+				comment: this.get('comment')
+			};
+
+			var comment = this.store.createRecord('comment', commentObj);
+
+			comment.save().then(function(data) {
+
+				if(data.error) {
+					// TODO error handler
+				} else {
+
+					self.set('fullname', '');
+					self.set('email', '');
+					self.set('comment', '');
+
+					self.get('model').get('comments').then(function(comments) {
+
+						comments.pushObject(data);
+					});
+
+					// TODO alertar o usuario do sucesso na insercao do comentario
+				}
+			});
 		}
 	}
 });
