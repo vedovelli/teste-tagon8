a84ff3af85b9939bf0a19080961c1e6c3bfa2297
Vedovelli <vedovelli@gmail.com>
2014-07-03 09:02:25 -0300

    comentários no código e mais clean up

--- src/js/router.js
+++ src/js/router.js
@@ -16,7 +16,7 @@ App.Router.map(function() {
 
 App.IndexRoute = Ember.Route.extend({
 
-	/* não há rota /#/ */
+	/* Redireciona para a lista de posts pois não há conteúdo para a rota index */
 	beforeModel: function() {
 
 		this.transitionTo('posts');
@@ -41,6 +41,7 @@ App.PostsNewRoute = Ember.Route.extend({
 
 	activate: function() {
 
+		/* esconde o botão de novo post pois não faz sentido mante-lo quando ja estamos no form de criação de novo post*/
 		this.controllerFor('posts').set('newPostButtonVisible', false);
 
 		/* se a rota /#/posts/new for acessada diretamente, verifica se o usuário está logado*/
@@ -56,7 +57,7 @@ App.AccountRoute = Ember.Route.extend({
 
 	activate: function() {
 
-		/* se a rota /#/account for acessada diretamente, verifica se o usuário está logado*/
+		/* se a rota /account for acessada diretamente, verifica se o usuário está logado*/
 		if(!this.controllerFor('login').get('isLoggedIn')) {
 
 			/* redirecionada para login caso não esteja logado  */
@@ -75,11 +76,13 @@ App.PostRoute = Ember.Route.extend({
 
 App.LoginRoute = Ember.Route.extend({
 
+	/* esconde o botão de novo post pois não faz sentido mante-lo quando ja estamos no form de login*/
 	activate: function() {
 
 		this.controllerFor('login').set('hideLoginButton', true);
 	},
 
+	/* mostra o botão de login ao sair da tela de login */
 	deactivate: function() {
 
 		this.controllerFor('login').set('hideLoginButton', false);
